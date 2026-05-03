67be4f12def15bdd656d8a0421aee25372aa951f
Yoni190 <yfisuhu@gmail.com>
2026-05-03 17:52:23 +0300

    Create route to register

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -4,6 +4,7 @@ const authController = require('../controllers/authController')
 const router = Router()
 
 router.post('/login', authController.localLogin)
+router.post('/register', authController.register)
 
 
 module.exports = router
