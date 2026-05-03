0432b1e96037376e18c1fe41b3289ace13f77887
Yoni190 <yfisuhu@gmail.com>
2026-05-03 16:01:58 +0300

    Add fields to user model

---
+++ server/prisma/migrations/20260503130138_add_fields_to_user/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[githubId]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `avatar` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `bio` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `githubId` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "avatar" TEXT NOT NULL,
+ADD COLUMN     "bio" TEXT NOT NULL,
+ADD COLUMN     "githubId" TEXT NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_githubId_key" ON "User"("githubId");
