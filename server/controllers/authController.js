65358a37e53d2a48d2df9b83ebebeb902f3c3a6c
Yoni190 <yfisuhu@gmail.com>
2026-05-03 17:52:15 +0300

    Create function to register

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,6 @@
 const passport = require('passport')
 const { issueTokens } = require('../helpers/tokenHelper')
+const { createUser } = require('../services/authService')
 
 const localLogin = (req, res, next) => {
     passport.authenticate('local', { session: false }, (err, user, info) => {
@@ -11,6 +12,18 @@ const localLogin = (req, res, next) => {
     })(req, res, next)
 }
 
+const register = async (req, res) => {
+    try {
+        await createUser(req.body)
+
+        return res.json({ message: 'User created successfully!' })
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({ message: 'Something went wrong' })
+    }
+}
+
 module.exports = {
-    localLogin
+    localLogin,
+    register
 }
