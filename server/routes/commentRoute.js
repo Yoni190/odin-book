253523660b72abe30600a5579088a6fa1839b439
Yoni190 <yfisuhu@gmail.com>
2026-05-05 09:22:17 +0300

    Create route to store comments

--- server/routes/commentRoute.js
+++ server/routes/commentRoute.js
@@ -6,6 +6,7 @@ const { authenticate } = require('../middleware/authenticate')
 const router = Router()
 
 router.get('/:id/comments', authenticate, commentController.index)
+router.post('/:id/comments', authenticate, commentController.store)
 
 
 module.exports = router
