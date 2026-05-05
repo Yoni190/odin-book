8fca4ede5e00d5ee05c8eb00e1b6c82c4912f752
Yoni190 <yfisuhu@gmail.com>
2026-05-05 08:55:00 +0300

    Create route to store likes

--- server/routes/likeRoute.js
+++ server/routes/likeRoute.js
@@ -5,6 +5,7 @@ const { authenticate } = require('../middleware/authenticate')
 const router = Router()
 
 router.get('/:id/likes', authenticate, likeController.index)
+router.post('/:id/likes', authenticate, likeController.store)
 
 
 module.exports = router
