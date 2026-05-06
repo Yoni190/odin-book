a133fa23e5bbd0eb23cc1275dc4cc7964ddd0566
Yoni190 <yfisuhu@gmail.com>
2026-05-06 15:49:22 +0300

    Create error for invalid data

--- server/lib/errors.js
+++ server/lib/errors.js
@@ -14,7 +14,16 @@ class ForbiddenError extends Error {
     }
 }
 
+class InvalidDataError extends Error {
+    constructor(message) {
+        super(message)
+        this.name = 'InvalidDataError'
+        this.statusCode = 422
+    }
+}
+
 module.exports = {
     NotFoundError,
-    ForbiddenError
+    ForbiddenError,
+    InvalidDataError
 }
