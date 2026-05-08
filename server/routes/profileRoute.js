d2ae0c0fb98faf6f6ddf53b893df6223dea536ba
Yoni190 <yfisuhu@gmail.com>
2026-05-08 19:11:25 +0300

    Create route to get info of a specific user

--- server/routes/profileRoute.js
+++ server/routes/profileRoute.js
@@ -6,6 +6,7 @@ const { authenticate } = require('../middleware/authenticate')
 const router = Router()
 
 router.get('/', authenticate, profileController.index)
+router.get('/:id', authenticate, profileController.getUserInfo)
 router.put('/', authenticate, profileController.update)
 
 module.exports = router
