40d6aafa385645194cd147617d0933e533e25fee
Yoni190 <yfisuhu@gmail.com>
2026-05-09 13:49:33 +0300

    Validate token in the server

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,11 +1,49 @@
+import axios from "axios"
+import { useEffect } from "react"
+import { useState } from "react"
 import { Navigate, Outlet } from "react-router"
 
 
 
-const ProtectedRoute = () => {
-  const token = localStorage.getItem('token')
 
-  return token ? <Outlet /> : <Navigate to='/' replace />
+const ProtectedRoute = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const [loading, setLoading] = useState(true)
+  const API_URL = import.meta.env.VITE_API_URL
+
+  useEffect(() => {
+    const verifyToken = async () => {
+        const token = localStorage.getItem('token')
+
+        if(!token) {
+            setLoading(false)
+            return
+        }
+
+        try {
+            await axios.get(`${API_URL}/auth/verify-token`, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            })
+
+            setIsAuthenticated(true)
+        } catch (error) {
+            localStorage.removeItem('token')
+            setIsAuthenticated(false)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    verifyToken()
+  }, [])
+  
+  if(loading) {
+    return <h1>Loading...</h1>
+  }
+
+  return isAuthenticated ? <Outlet /> : <Navigate to='/' replace />
 }
 
 export default ProtectedRoute
