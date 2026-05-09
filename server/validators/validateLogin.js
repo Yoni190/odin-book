521c62bceb5a8e479fd085640866319cb1d3e1c4
Yoni190 <yfisuhu@gmail.com>
2026-05-09 13:23:24 +0300

    Validate existence of user

--- server/validators/validateLogin.js
+++ server/validators/validateLogin.js
@@ -1,11 +1,21 @@
 const { body } = require('express-validator')
+const { prisma } = require('../lib/prisma')
 
 
 const validateLogin = [
     body('username')
         .trim()
         .notEmpty()
-        .withMessage('Username should not be empty.'),
+        .withMessage('Username should not be empty.')
+        .custom(async (username) => {
+            const user = await prisma.user.findUnique({
+                where: { username }
+            })
+
+            if(!user) {
+                throw new Error('User not found.')
+            }
+        }),
     
     body('password')
         .trim()
