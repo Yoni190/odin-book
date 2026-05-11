5e3b77dd426cff765f47d7497fd218e06d6dd76e
Yoni190 <yfisuhu@gmail.com>
2026-05-11 16:34:09 +0300

    Add logout button

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { useNavigate } from 'react-router'
 
 const Header = () => {
+    const navigate = useNavigate()
+
+    const handleLogout = () => {
+        localStorage.removeItem('token')
+        navigate('/')
+    }
+
   return (
-    <header>
-        <h1>Header</h1>
+    <header className='bg-red-800 text-white p-4 flex justify-between'>
+        <h1 className='text-4xl font-semibold'>Clover</h1>
+
+        <button
+            className='border p-1 rounded cursor-pointer hover:bg-white hover:text-red-800 transition'
+            onClick={handleLogout}
+        >
+            Log Out
+        </button>
     </header>
   )
 }
